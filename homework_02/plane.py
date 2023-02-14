a33d146b5f10835cf3fb3c1ce4b9aab47834b8ff
lizok <esillicher@gmail.com>
2023-02-14 12:44:50 +0800

    add max_cargo in init

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -7,14 +7,15 @@ from exceptions import CargoOverload
 
 
 class Plane(Vehicle):
-    def __init__(self, weight=1000, fuel=0, fuel_consumption=1.5):
+    def __init__(self, weight: float, fuel: float, fuel_consumption: float, max_cargo: float):
         super().__init__(weight, fuel, fuel_consumption)
-        self.cargo = None
+        self.max_cargo = 10
+        self.cargo = 0
 
-    def __int__(self, cargo: float, max_cargo: float,  *args, **kwargs):
-        self.cargo = cargo
-        self.max_cargo = max_cargo
-        super().__init__(*args, **kwargs)
+    # def __init__(self, cargo: float, max_cargo: float,  *args, **kwargs):
+    #     self.cargo = cargo
+    #     self.max_cargo = max_cargo
+    #     super().__init__(*args, **kwargs)
 
     def load_cargo(self, added_cargo: float):
         if added_cargo + self.cargo < self.max_cargo:
@@ -24,7 +25,12 @@ class Plane(Vehicle):
 
         return self.cargo
 
-    def remove_all_cargo(self, prev_cargo):
+    def remove_all_cargo(self):
         prev_cargo = self.cargo
         self.cargo = 0
         return prev_cargo
+
+
+# plane = Plane(100,100,19,1)
+# plane = Plane(weight=5375, fuel=3520, fuel_consumption=7151, started=False)
+
