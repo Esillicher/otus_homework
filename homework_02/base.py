2f0024fff644fc1a0bc2db1b01a8514962a8ebee
lizok <esillicher@gmail.com>
2023-02-14 11:19:52 +0800

    add Vehicle class with attributes and methods

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,53 @@
 from abc import ABC
+import exceptions
 
 
 class Vehicle(ABC):
-    pass
+    def __init__(self, weight=1000, fuel=0, fuel_consumption=1.5):
+        self.weight = weight
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+
+    started: bool = False
+
+    def __str__(self):
+        return f"{type(self).__name__}(weight={self.weight!r}, fuel={self.fuel}," \
+               f" fuel_consumption={self.fuel_consumption}, started={self.started})"
+
+    def __repr__(self):
+        return str(self)
+
+    def start(self) -> bool:
+        if self.started:
+            print('engine already started')
+        else:
+            if self.fuel == 0:
+                raise exceptions.LowFuelError(f"not enough fuel for start")
+            else:
+                print('successfully started')
+                self.started = True
+
+        return self.started
+
+    def move(self, distance: float):
+        needed_fuel = self.fuel_consumption * distance / 100
+
+        if needed_fuel < self.fuel:
+            print("let's go!")
+            self.fuel = self.fuel - needed_fuel
+        else:
+            raise exceptions.NotEnoughFuel(f"not enough fuel for journey")
+
+        return self.fuel
+
+
+scoopy = Vehicle()
+scoopy.started = True
+print(scoopy)
+print(scoopy.start())
+#print(scoopy.move(0.1))
+scoopy.fuel = 5
+print(scoopy)
+print(scoopy.move(1))
+
+
